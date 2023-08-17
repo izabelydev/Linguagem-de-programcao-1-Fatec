190e1b81e48c54b736cd55268f33542b91e56cca
izalrnc <izabelylrnc@gmail.com>
2023-08-16 21:27:09 -0300

    Aula de linguagem de programação: criando um niquel usando c# no Forms

---
+++ NiquelC#Forms/niquelnoite/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace niquelnoite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblpontos = new System.Windows.Forms.Label();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.btjogar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblpontos
+            // 
+            this.lblpontos.AutoSize = true;
+            this.lblpontos.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblpontos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblpontos.Location = new System.Drawing.Point(415, 53);
+            this.lblpontos.Name = "lblpontos";
+            this.lblpontos.Size = new System.Drawing.Size(13, 13);
+            this.lblpontos.TabIndex = 0;
+            this.lblpontos.Text = "0";
+            this.lblpontos.Click += new System.EventHandler(this.Lblpontos_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(89, 107);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(185, 157);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1.TabIndex = 1;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(301, 107);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(185, 157);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic2.TabIndex = 2;
+            this.pic2.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(503, 107);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(185, 157);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic3.TabIndex = 3;
+            this.pic3.TabStop = false;
+            // 
+            // btjogar
+            // 
+            this.btjogar.Location = new System.Drawing.Point(353, 306);
+            this.btjogar.Name = "btjogar";
+            this.btjogar.Size = new System.Drawing.Size(75, 23);
+            this.btjogar.TabIndex = 4;
+            this.btjogar.Text = "Jogar";
+            this.btjogar.UseVisualStyleBackColor = true;
+            this.btjogar.Click += new System.EventHandler(this.Btjogar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pontuação:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btjogar);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.lblpontos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblpontos;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.Button btjogar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NiquelC#Forms/niquelnoite/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace niquelnoite
+{
+    public partial class Form1 : Form
+    {
+        int qtde = 0, pontos = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btjogar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+        }
+
+        private void Lblpontos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Random sorteio = new Random(); //Random gera números aleatórios
+            int num1, num2, num3;
+            num1 = sorteio.Next(1, 7); num2 = sorteio.Next(1, 7); num3 = sorteio.Next(1, 7);
+
+
+            //encontra as imagens sorteadas que estão salvas no executavel
+            pic1.Load(Application.StartupPath + "\\frutas\\" + num1 + ".png");
+            pic2.Load(Application.StartupPath + "\\frutas\\" + num2 + ".png");
+            pic3.Load(Application.StartupPath + "\\frutas\\" + num3 + ".png");
+
+            qtde = qtde + 1;
+                if (qtde > 10)
+                {
+                    timer1.Enabled = false;
+                    qtde = 0;            
+                
+                    //verifica se acertou as 3 imagens
+                    if (num1 == num2 && num2 == num3 && num1 == num3)
+                    {
+                        num1 = 5; //dificulta o jogo
+                        pontos = pontos + 10; //ganha pontos
+                        lblpontos.Text = pontos.ToString();
+                    }
+                        else if(num1 == num2 || num2 == num3 || num1 == num3)
+                        {
+                            pontos = pontos + 1;
+                            lblpontos.Text = pontos.ToString();
+                        }
+                            else if (num1 != num2 && num2 != num3 && num1 != num3)
+                            {
+                                pontos = pontos - 3;
+                                lblpontos.Text = pontos.ToString();
+                                if (pontos <=0 )
+                                {
+                                    pontos = 0;
+                                    lblpontos.Text = pontos.ToString();
+                                }
+                            }
+
+            }
+        }
+    }
+}
