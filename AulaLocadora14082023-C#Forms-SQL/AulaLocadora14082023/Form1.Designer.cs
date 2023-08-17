b583fce6fc604e9fe73342b90902a14e0fa7862e
izalrnc <izabelylrnc@gmail.com>
2023-08-16 21:47:35 -0300

    Aula C# no Forms para mostrar dados armazenados no Banco de Dados

---
+++ AulaLocadora14082023-C#Forms-SQL/AulaLocadora14082023/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace AulaLocadora14082023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btEntrar = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btEntrar
+            // 
+            this.btEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEntrar.Location = new System.Drawing.Point(70, 348);
+            this.btEntrar.Name = "btEntrar";
+            this.btEntrar.Size = new System.Drawing.Size(116, 37);
+            this.btEntrar.TabIndex = 0;
+            this.btEntrar.Text = "Entrar";
+            this.btEntrar.UseVisualStyleBackColor = true;
+            this.btEntrar.Click += new System.EventHandler(this.BtEntrar_Click);
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancelar.Location = new System.Drawing.Point(206, 348);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(116, 37);
+            this.btCancelar.TabIndex = 1;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.BtCancelar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha:";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.Location = new System.Drawing.Point(75, 231);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(21, 20);
+            this.lblMsg.TabIndex = 4;
+            this.lblMsg.Text = "...";
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.Location = new System.Drawing.Point(153, 151);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.Size = new System.Drawing.Size(143, 20);
+            this.txtsenha.TabIndex = 5;
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(153, 93);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(143, 20);
+            this.txtusuario.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 450);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.txtsenha);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.btEntrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btEntrar;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.TextBox txtusuario;
+    }
+}
+
---
+++ AulaLocadora14082023-C#Forms-SQL/AulaLocadora14082023/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaLocadora14082023
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conexao;
+        private SqlDataAdapter adapter;
+        private DataTable tblFuncionarios;
+        private String strsql, strconex;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtCancelar_Click(object sender, EventArgs e)
+        {
+            txtusuario.Text = null;
+            txtsenha.Text = null;
+        }
+
+        private void BtEntrar_Click(object sender, EventArgs e)
+        {
+            strconex = "data source=(local); initial catalog=locadora; integrated security=sspi";
+            conexao = new SqlConnection(strconex);
+            conexao.Open();
+
+            tblFuncionarios = new DataTable();
+
+            strsql = "select * from funcionarios where usuario='" +txtusuario.Text+ "' and senha ='" +txtsenha.Text+ "'";
+
+            adapter = new SqlDataAdapter(strsql, strconex);
+            adapter.Fill(tblFuncionarios);
+
+            if(tblFuncionarios.Rows.Count ==1)
+            {
+                lblMsg.Text = "Login efetuado com sucesso";
+            }
+            else
+            {
+                lblMsg.Text = "Erro ao efetuar login, tente novamente";
+            }
+        }
+    }
+}
